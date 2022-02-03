ec77675af8cb1f9519e3a02c9ed596e94ff098f3
louis-ui <1640974715@qq.com>
2022-02-03 16:08:30 +0800

    2022/2/3

--- src/剑指offer/_10- I_斐波那契数列.kt
+++ src/剑指offer/_10- I_斐波那契数列.kt
@@ -17,17 +17,17 @@ class `_10- I_斐波那契数列` {
 //    }
 //}
 
-class Solution {
-    fun fib(n: Int): Int {
-        if (n == 0) {
-            return 0
-        } else if (n == 1) {
-            return 1
-        } else {
-            return fib(n - 1) + fib(n - 2)
-        }
-    }
-}
+//class Solution {
+//    fun fib(n: Int): Int {
+//        if (n == 0) {
+//            return 0
+//        } else if (n == 1) {
+//            return 1
+//        } else {
+//            return fib(n - 1) + fib(n - 2)
+//        }
+//    }
+//}
 
 
 //fun main() {
